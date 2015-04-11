e04f324f2f8af087cc3a17617119350e3b60bf01
Alexandru Coman <acoman@cloudbasesolutions.com>
2015-04-11 23:39:12 +0300

    Update the ec-setup.sh

--- ec-azure-experimental/provision/ec-setup.sh
+++ ec-azure-experimental/provision/ec-setup.sh
@@ -9,7 +9,23 @@ ORGANIZATION=""     # Organization Name (eg, company)
 NAME=""             # Common Name (e.g. server FQDN or YOUR name)
 EMAIL=""            # Email Address
 
-CERT_DIR="/home/vagrant/.ssh/"
+SUBSCRIPTION=""
+LOCATION="East US"
+SERVICE_NAME="ec-azure-service"
+STORAGE_ACCOUNT="ec-azure-storage"
+DEPLOYMENT="ec-azure-deployment"
+IMAGE_ID="b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu-12_04_2-LTS-amd64-server-20121218-en-us-30GB"
+FRONTEND_NODES="1"
+COMPUTE_NODES="6"
+FLAVOR="Small"
+
+MANAGEMENT_CER="managementCert.cer"
+MANAGEMENT_PEM="managementCert.pem"
+CLIENT_KEY="azureClient.key"
+CLIENT_RSA_KEY="azureClientRSA.key"
+CLIENT_PEM="azureClient.pem"
+
+CERT_DIR="/home/vagrant/.ssh"
 SITE_PACKAGES="/home/vagrant/.venv/ec-azure-experimental/lib/python2.7/site-packages"
 AZURE_EXPERIMENTAL="/home/vagrant/elasticluster/azure-experimental"
 
@@ -51,9 +67,8 @@ function patch_libs() {
     git checkout master
 }
 
-function management_certificate() {
-    echo "Generate the management certificate for Azure"
-    subject=""
+function get_subject() {
+    local subject=""
 
     if [ $COUNTRY ]; then
         subject+="/C=$COUNTRY"
@@ -70,11 +85,18 @@ function management_certificate() {
     if [ $EMAIL ]; then
         subject+="/emailAddress=$EMAIL"
     fi
+    echo "$subject"
+}
 
+function management_certificate() {
+    echo "Generating the management certificate for Azure"
+    
+    local subject=$(get_subject)
     if [ ! subject ]; then
         echo "Failed to generate the management certificate."
         return -1
     fi
+
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
         -keyout /home/vagrant/.ssh/managementCert.pem \
         -out /home/vagrant/.ssh/managementCert.pem \
@@ -84,15 +106,73 @@ function management_certificate() {
         -out /home/vagrant/.ssh/managementCert.cer > /dev/null
 }
 
+function client_ssh_keys() {
+    echo "Generating the client ssh keys."
+
+    local subject=$(get_subject)
+    if [ ! subject ]; then
+        echo "Failed to generate the management certificate."
+        return -1
+    fi
+
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout ~/.ssh/azureClient.key \
+        -out ~/.ssh/azureClient.pem \
+        -subj "$subject" &> /dev/null
+    openssl rsa \
+        -in ~/.ssh/azureClient.key \
+        -out ~/.ssh/azureClientRSA.key &> /dev/null
+}
+
+function create_config(){
+    echo "Creating elasticluster config"
+    local template="#Elasticluster - Azure experimental
+[cloud/azure-cloud]
+provider=azure
+subscription_id=$SUBSCRIPTION
+certificate=$CERT_DIR/$MANAGEMENT_PEM
+
+[login/azure-login]
+image_user=ecazure
+image_user_sudo=root
+image_sudo=True
+
+# keypair used to run stuff on the nodes.
+user_key_name=$CLIENT_KEY
+user_key_private=$CERT_DIR/$CLIENT_RSA_KEY
+user_key_public=$CERT_DIR/$CLIENT_PEM
+
+[setup/ansible-gridengine-azure]
+provider=ansible
+frontend_groups=gridengine_master
+compute_groups=gridengine_clients
+
+[cluster/azure-gridengine]
+cloud=azure-cloud
+login=azure-login
+setup_provider=ansible-gridengine-azure
+cloud_service_name=$SERVICE_NAME
+location=$LOCATION
+frontend_nodes=$FRONTEND_NODES
+compute_nodes=$COMPUTE_NODES
+ssh_to=frontend
+image_id=$IMAGE_ID
+flavor=$FLAVOR
+storage_account_name=$STORAGE_ACCOUNT
+security_group=default
+deployment_name=$DEPLOYMENT
+global_var_ansible_ssh_host_key_dsa_public=''"
+    echo "$template" > ~/azure.conf
+}
+
 function epilogue() {
     echo "Done!"
 }
-
-# TODO(alexandrucoman): Generate the client ssh keys
-# TODO(alexandrucoman): Generate the config file
 
 prologue
 install_elasticluster
 patch_libs
 management_certificate
+client_ssh_keys
+create_config
 epilogue
