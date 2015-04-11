55e3a6ea108386366b6580fce68faf583dd738bc
Alexandru Coman <acoman@cloudbasesolutions.com>
2015-04-11 21:48:08 +0300

    Refactor ec-azure-experimental environment

--- ec-azure-experimental/provision/ec-setup.sh
+++ ec-azure-experimental/provision/ec-setup.sh
@@ -1,36 +1,98 @@
 #!/bin/bash
 set -e
 
+# Settings
+COUNTRY=""          # Country Name (2 letter code)
+STATE=""            # State or Province Name (full name)
+LOCALITY=""         # Locality Name (eg, city)
+ORGANIZATION=""     # Organization Name (eg, company)
+NAME=""             # Common Name (e.g. server FQDN or YOUR name)
+EMAIL=""            # Email Address
+
+CERT_DIR="/home/vagrant/.ssh/"
 SITE_PACKAGES="/home/vagrant/.venv/ec-azure-experimental/lib/python2.7/site-packages"
 AZURE_EXPERIMENTAL="/home/vagrant/elasticluster/azure-experimental"
 
-echo "Creating the ec-azure-experimental virtualenv"
-virtualenv --python=/usr/bin/python2.7 /home/vagrant/.venv/ec-azure-experimental
+function prologue() {
+    if [ ! -d "$CERT_DIR" ]; then
+      echo "Create $CERT_DIR"
+      mkdir $CERT_DIR
+    fi
+}
 
-echo "Activating .venv/ec-azure-experimental"
-source /home/vagrant/.venv/ec-azure-experimental/bin/activate
+function install_elasticluster() {
+    echo "Creating the ec-azure-experimental virtualenv"
+    virtualenv --python=/usr/bin/python2.7 /home/vagrant/.venv/ec-azure-experimental
 
-echo "Installing elasticluster requirements"
-pip install pip --upgrade > /dev/null
-pip install ansible==1.7.2 azure google-api-python-client &> /dev/null
+    echo "Activating .venv/ec-azure-experimental"
+    source /home/vagrant/.venv/ec-azure-experimental/bin/activate
 
-echo "Cloning elasticluster"
-git clone https://github.com/bobd00/elasticluster &> /dev/null
+    echo "Installing elasticluster requirements"
+    pip install -q pip --upgrade > /dev/null
+    pip install -q ansible==1.7.2 azure google-api-python-client &> /dev/null
 
-echo "Installing elasticluster"
-cd elasticluster
-python setup.py install &> /dev/null
+    echo "Cloning elasticluster"
+    git clone https://github.com/bobd00/elasticluster
 
-echo "Patching ansible.runner.__init__"
-git checkout azure-experimental
-cp "$AZURE_EXPERIMENTAL/ansible/runner/__init__.py" "$SITE_PACKAGES/ansible/runner/__init__.py"
-git checkout master
+    echo "Installing elasticluster"
+    cd /home/vagrant/elasticluster
+    python setup.py install -q &> /dev/null
+}
 
-echo "Patching azure.servicemanagement.__init__"
-sed -i 's/if configuration.public_ips:/if False:/g' "$SITE_PACKAGES/azure/servicemanagement/__init__.py"
+function patch_libs() {
+    git checkout azure-experimental
 
-# TODO(alexandrucoman): Generate the management certificate for Azure
+    echo "Patching azure.servicemanagement.__init__"
+    sed -i 's/if configuration.public_ips:/if False:/g' "$SITE_PACKAGES/azure/servicemanagement/__init__.py"
+
+    echo "Patching ansible.runner.__init__"
+    cp "$AZURE_EXPERIMENTAL/ansible/runner/__init__.py" "$SITE_PACKAGES/ansible/runner/__init__.py"
+
+    git checkout master
+}
+
+function management_certificate() {
+    echo "Generate the management certificate for Azure"
+    subject=""
+
+    if [ $COUNTRY ]; then
+        subject+="/C=$COUNTRY"
+    fi
+    if [ $STATE ]; then
+        subject+="/ST=$STATE"
+    fi
+    if [ $ORGANIZATION ]; then
+        subject+="/O=$ORGANIZATION"
+    fi
+    if [ $CNAME ]; then
+        subject+="/CN=$NAME"
+    fi
+    if [ $EMAIL ]; then
+        subject+="/emailAddress=$EMAIL"
+    fi
+
+    if [ ! subject ]; then
+        echo "Failed to generate the management certificate."
+        return -1
+    fi
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout /home/vagrant/.ssh/managementCert.pem \
+        -out /home/vagrant/.ssh/managementCert.pem \
+        -subj "$subject" &> /dev/null
+    openssl x509 -outform der \
+        -in /home/vagrant/.ssh/managementCert.pem \
+        -out /home/vagrant/.ssh/managementCert.cer > /dev/null
+}
+
+function epilogue() {
+    echo "Done!"
+}
+
 # TODO(alexandrucoman): Generate the client ssh keys
 # TODO(alexandrucoman): Generate the config file
 
-echo "Done!"
+prologue
+install_elasticluster
+patch_libs
+management_certificate
+epilogue
--- ec-azure-experimental/provision/vagrant.sh
+++ ec-azure-experimental/provision/vagrant.sh
@@ -6,6 +6,6 @@ apt-get update -qy > /dev/null
 apt-get upgrade -qy > /dev/null
 
 echo "Installing requirede packages"
-apt-get install -y build-essential python-dev git python-pip libssl-dev libffi-dev nodejs-legacy npm > /dev/null
-npm install -g azure-cli > /dev/null
+apt-get install -qy build-essential python-dev git python-pip libssl-dev libffi-dev nodejs-legacy npm &> /dev/null
+npm install -gq azure-cli &> /dev/null
 pip install virtualenv &> /dev/null
