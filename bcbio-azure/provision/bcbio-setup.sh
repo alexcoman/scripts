132e1521968f1baaef9f04a2daae57ca49771bfa
Alexandru Coman <acoman@cloudbasesolutions.com>
2015-06-07 02:13:49 +0300

    Install bcbio-nextgen-vm and its requirements

--- bcbio-azure/provision/bcbio-setup.sh
+++ bcbio-azure/provision/bcbio-setup.sh
@@ -2,7 +2,7 @@
 set -e
 
 function load_config() {
-	CONFIG_PATH='./default.conf'
+	CONFIG_PATH='/vagrant/provision/default.conf'
 	CONFIG_SYNTAX="^\s*#|^\s*$|^[a-zA-Z_]+='[^']*'$"
 
 	if egrep -q -v "${CONFIG_SYNTAX}" "$CONFIG_PATH"; then
@@ -14,10 +14,17 @@ function load_config() {
 	source "${CONFIG_PATH}"
 }
 
+function pip_cache() {
+	if [ ! -d "$PIP_CACHE" ]; then
+		sudo mkdir -p "$PIP_CACHE"
+	fi
+}
+
 function install_bcbio() {
 	cd
 	if [ -d "$BCBIOVM_PATH" ]; then
 		echo "Removing the old version of bcbio-nextgen-vm."
+		sudo pip uninstall bcbiovm
 		sudo rm -rf bcbio-nextgen-vm &> /dev/null
 	fi
 	echo "Cloning the bcbio-nextgen-vm project."
@@ -25,10 +32,30 @@ function install_bcbio() {
 
 	cd "$BCBIOVM_PATH"
 	echo "Installing bcbio-nextgen-vm requirements."
-	sudo pip install -r requirements.txt &> /dev/null
+	sudo pip install -r requirements.txt --upgrade --cache-dir "$PIP_CACHE" &> /dev/null
+	echo "Installing pybedtools in order to avoid MemoryError."
+	sudo pip install pybedtools>=0.6.8 &> /dev/null
 	echo "Installing the bcbio-nextgen-vm project."
 	sudo python setup.py install &> /dev/null
 }
 
+function install_ansible() {
+	echo "Remove the current version of ansible."
+	sudo pip uninstall ansible &> /dev/null
+	echo "Install azure-ansible."
+	sudo pip install --pre azure-ansible &> /dev/null
+}
+
+function install_elasticluster() {
+	echo "Remove the current version of elasticluster."
+	sudo pip uninstall elasticluster &> /dev/null
+	echo "Install azure-elasticluster."
+	sudo pip install --pre azure-elasticluster &> /dev/null
+}
+
+
 load_config
+pip_cache
 install_bcbio
+install_ansible
+install_elasticluster
--- bcbio-azure/provision/vagrant.sh
+++ bcbio-azure/provision/vagrant.sh
@@ -1,48 +1,51 @@
 #!/bin/bash
 set -e
 
+echo "Cleanup the environment"
+apt-get remove -y python-six &> /dev/null 
+
 echo "Update the system."
 apt-get update -y &> /dev/null
 apt-get upgrade -y &> /dev/null
 
+echo "Installing pip."
+wget https://bootstrap.pypa.io/get-pip.py &> /dev/null
+chmod +x get-pip.py
+python get-pip.py &> /dev/null
+rm get-pip.py
+pip install setuptools --upgrade &> /dev/null
+pip install pip --upgrade &> /dev/null
+
 echo "Installing required packages."
-apt-get install -y git screen vim &> /dev/null
-apt-get install -y build-essential python-dev python-pip &> /dev/null
+apt-get install -qqy htop git &> /dev/null
+apt-get install -qqy build-essential python-dev &> /dev/null
 
 echo "Installing requirements for openssl."
-apt-get install -y libssl-dev libffi-dev &> /dev/null
+apt-get install -qqy libssl-dev libffi-dev &> /dev/null
 
 echo "Installing requirements for pycurl."
-apt-get install -y libcurl4-openssl-dev &> /dev/null
+apt-get install -qqy libcurl4-openssl-dev &> /dev/null
 
 echo "Installing requirements for lxml."
-apt-get install -y libxml2-dev libxslt1-dev &> /dev/null
+apt-get install -qqy libxml2-dev libxslt1-dev &> /dev/null
 
 echo "Installing requirements for matplotlib."
-apt-get install -y libxft-dev libpng-dev libfreetype6-dev &> /dev/null
+apt-get install -qqy libxft-dev libpng-dev libfreetype6-dev &> /dev/null
 
 echo "Installing requirements for scipy."
-apt-get install -y liblapack-dev gfortran &> /dev/null
-apt-get install -y libgmp-dev libmpfr-dev &> /dev/null
-apt-get install -y libblas-dev libblas3gf &> /dev/null
+apt-get install -qqy liblapack-dev gfortran &> /dev/null
+apt-get install -qqy libgmp-dev libmpfr-dev &> /dev/null
+apt-get install -qqy libblas-dev libblas3gf &> /dev/null
 
 echo "Installing requirements for matplotlib"
-apt-get install -y inkscape libav-tools gdb mencoder dvipng &> /dev/null
-apt-get install -y graphviz &> /dev/null
+apt-get install -qqy inkscape libav-tools gdb mencoder dvipng &> /dev/null
+apt-get install -qqy graphviz &> /dev/null
 
 echo "Installing requirements for pysam"
-apt-get install -y gcc g++ zlib1g-dev libbz2-dev libpng12-dev &> /dev/null
-apt-get install -y libatlas-dev libpq-dev r-base-dev libreadline-dev &> /dev/null
-apt-get install -y libmysqlclient-dev libboost-dev libsqlite3-dev &> /dev/null
-
-echo "Installing requirements for biopython"
-apt-get install -y clustalo &> /dev/null
-apt-get install -y muscle mafft probcons wise emboss &> /dev/null
-apt-get install -y samtools bwa &> /dev/null
+apt-get install -qqy gcc g++ zlib1g-dev libbz2-dev libpng12-dev &> /dev/null
+apt-get install -qqy libatlas-dev libpq-dev r-base-dev libreadline-dev &> /dev/null
+apt-get install -qqy libmysqlclient-dev libboost-dev libsqlite3-dev &> /dev/null
 
 echo "Installing azure client."
 apt-get install -y nodejs-legacy npm &> /dev/null
 npm install -gq azure-cli &> /dev/null
-
-echo "Upgrading pip."
-pip install pip --upgrade &> /dev/null
