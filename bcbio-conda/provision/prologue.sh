8c45134715b2f394258c3ae2abeff28ca80806f7
Cosmin Poieana <cmin@ropython.org>
2015-07-01 16:51:48 +0300

    Deploy bcbio through Miniconda

---
+++ bcbio-conda/provision/prologue.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Installing do2unix"
+sudo apt-get install dos2unix &> /dev/null
+
+echo "Change the line ending for the files from /vagrant"
+dos2unix /vagrant/provision/vagrant.sh
