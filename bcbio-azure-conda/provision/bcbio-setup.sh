d948c3b7dcdbcefabbdb49c27a66127025cbfeb7
Alexandru Coman <acoman@cloudbasesolutions.com>
2015-11-19 01:36:20 +0200

    Remove deprecated Vagrantfile

--- bcbio-azure-conda/provision/bcbio-setup.sh
+++ bcbio-azure-conda/provision/bcbio-setup.sh
@@ -23,16 +23,8 @@ function add_channels() {
 }
 
 function install_bcbio() {
-    echo "Installing elasticluster"
-    conda install --yes --quiet --channel "$DEV_CHANNEL" elasticluster --force &> /dev/null
-    echo "Done"
-
-    echo "Installing bcbio-nextgen..."
-    conda install --yes --quiet --channel "$DEV_CHANNEL" bcbio-nextgen --force &> /dev/null
-    echo "Done."
-
     echo "Installing bcbio-nextgen-vm..."
-    conda install --yes --quiet --channel "$DEV_CHANNEL" bcbio-nextgen-vm --force &> /dev/null
+    conda install bcbio-nextgen-vm --yes --quiet --channel "$DEV_CHANNEL" &> /dev/null
     echo "Done."
 }
 
